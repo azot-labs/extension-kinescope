1284852e127c2de26cf1cbc8b723def60b958c04
Vitaly Gashkov <30000398+vitalygashkov@users.noreply.github.com>
2026-06-06 20:35:21 +0500

    fix: explicitly ask for referer

--- kinescope.js
+++ kinescope.js
@@ -1,4 +1,4 @@
-import { defineExtension } from "azot";
+import { defineExtension, prompt } from "azot";
 
 const KINESCOPE_BASE_URL = "https://kinescope.io";
 const KINESCOPE_MASTER_PLAYLIST_URL = "https://kinescope.io/{video_id}/master.mpd";
@@ -20,15 +20,33 @@ const DEFAULT_REFERER = "https://kinescope.io";
  * Command: streamyx https://kinescope.io/embed/202544377
  */
 
+const getReferer = async (headers) => {
+  const referer = headers.referer || headers.Referer;
+  if (referer) return referer;
+  const response = await prompt("Enter the website where the Kinescope player is located", {
+    form: {
+      referer: {
+        type: "text",
+        title: "Referer",
+        autoFocus: true,
+      },
+    },
+  });
+  return `${new URL(response.referer ?? DEFAULT_REFERER).origin}/`;
+};
+
 export default defineExtension({
   async fetchContentMetadata(url, args) {
-    const headers = args.header;
-    const response = await fetch(url, { headers });
+    const headers = args.header ?? {};
+    const referer = await getReferer(headers);
+    const response = await fetch(url, { headers: { referer } });
     const data = await response.text();
 
     const title = data.split("<title>")[1]?.split("</title>")[0];
     const playerOptionsString = data.split("playerOptions = ")[1]?.split("};")[0] + "}";
     const playerOptions = eval(`(${playerOptionsString})`);
+
+    if (!playerOptions?.playlist) return [];
 
     const selectedEpisodes = args.episodes
       ? Array.from(args.episodes.values()).flatMap((seasonEpisodes) =>
